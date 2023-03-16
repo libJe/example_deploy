cc39f6a8a0e6bc84de77a1dfff53fd881f038c73
sokritha-it <sokritha.it@gmail.com>
2023-03-16 16:46:20 +0700

    server: add new route /

--- api/server.js
+++ api/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
+
 app.listen(8080, () => {
   console.log("listening on port 8080");
 });
